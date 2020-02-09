b0b88c00984a6d47c59ee82118ba8242647409de
francisco guilherme <franciscoguilherme.v@gmail.com>
2020-02-09 17:41:47 -0300

    página inicial
    estruturando diretório de views;
    definindo primeiras rotas;

---
+++ cadastroClientes/resources/views/pages/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('content')
+
+<h1>Página Inicial</h1>
+<p class="lead">CRUD Cadastro de Clientes utilizando Laravel</p>
+<hr>
+
+<a href="{{ url('clientes') }}" class="btn btn-info">Ver clientes</a>
+<a href="{{ url('clientes/create') }}" class="btn btn-primary">Add novo cliente</a>
+
+@stop
